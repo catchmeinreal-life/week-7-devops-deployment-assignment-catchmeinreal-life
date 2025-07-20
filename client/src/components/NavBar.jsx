e2f9179cd7478132ec8fbc560ab1d7c6f800cd1d
ERIC MATUTU <ericmatutu125@mail.com>
2025-07-21 02:34:41 +0300

    not importing the css

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Menu, X } from "lucide-react";
-import "../styles/navbar.css";
 
 function NavBar() {
   const location = useLocation();
