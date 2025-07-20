4f4f79a2fe24f2117f322aead778815fbd13547b
ERIC MATUTU <ericmatutu125@mail.com>
2025-07-21 02:27:21 +0300

    BUG FIX: misspelling of css import..

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Menu, X } from "lucide-react";
-import "../styles/navBar.css";
+import "../styles/navbar.css";
 
 function NavBar() {
   const location = useLocation();
