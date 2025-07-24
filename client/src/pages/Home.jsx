bc3fb25dd228b97b4448c09f87ec8f3ca22a4f87
ERIC MATUTU <ericmatutu125@mail.com>
2025-07-24 08:31:16 +0300

    initial layout of the Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,36 +1,23 @@
-import { useState } from 'react';
-import reactLogo from '../assets/react.svg';
-import viteLogo from '../../public/vite.svg';
+import { Link } from "react-router-dom"
+import { HandCoins, Bitcoin } from 'lucide-react';
+import Navbar from '../components/NavBar';
+
 import '../App.css';
 
-import Navbar from '../components/Navbar';
-
 export default function Home() {
-    const [count, setCount] = useState(0)
+
 
     return (
         <>
           <Navbar />
-          <div>
-            <a href="https://vite.dev" target="_blank">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
+          <div className='container icons'>
+            <Link to='/'>
+                <HandCoins />
+            </Link>
+            <Link to='/'>
+                <Bitcoin />
+            </Link>
           </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-            <p>
-              Edit <code>src/App.jsx</code> and save to test HMR
-            </p>
-          </div>
-          <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-          </p>
         </>
       )
 }
