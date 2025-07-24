490ca0c2a6db5135e3c6bd704e7e1204a9491d44
ERIC MATUTU <ericmatutu125@mail.com>
2025-07-24 14:53:12 +0300

    new changes>> data is now being stored in the mern-auth database instead on of the default test

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,8 +1,18 @@
 import mongoose from "mongoose";
 
+// 
 const connectDB = async () => {
-    mongoose.connection.on('connected', () => console.log("Connected to the database"));
-    await mongoose.connect(`${process.env.MONGO_URL}/mern-auth`);
+    try {
+        mongoose.connection.on('connected', () => console.log("Connected to the database"));
+        await mongoose.connect(`${process.env.MONGO_URI}/mern-auth`);
+
+        console.log(mongoose.connection.name);
+        
+    } catch (error) {
+        console.log("database connection error");
+        console.error(error.message);
+        process.exit(1);
+    }
 };
 
 export default connectDB;
