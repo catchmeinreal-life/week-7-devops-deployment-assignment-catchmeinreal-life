5dbe1a77fdb57f3cb42f2eff2966171107164106
ERIC MATUTU <ericmatutu125@mail.com>
2025-07-24 11:48:25 +0300

    added login logic - modified registration logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -40,9 +40,51 @@ export const register = async (req, res) => {
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 1 * 24 * 60 * 60 * 1000
-        })
+        });
+        return res.json({success: true});
         
     } catch (error) {
-        res.json({succes: false, message: error.message})
+        return res.json({succes: false, message: error.message})
     }
+}
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    if (!email || !password) {
+        return res.json({succes: false, message: 'Email and Password are required'});
+    }
+
+    try {
+
+        const user = await userModel.findOne({email});
+        if(!user) {
+            return res.json({success: false, message: 'Invalid Email'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.json({success: false, message: 'Invalid Password'});
+        }
+
+        /**
+         * generate token for authentication
+         * and send it via cookies
+         */
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '1d'});
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 1 * 24 * 60 * 60 * 1000
+        })
+
+        return res.json({success: true});
+        
+        
+    } catch (error) {
+        return res.json({success: false, message: error.message});
+    }
+
 }
