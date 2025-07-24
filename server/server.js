8c408f6c26c41482df672d481636503115034833
ERIC MATUTU <ericmatutu125@mail.com>
2025-07-24 12:02:09 +0300

    created endpoints imported authRoutes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 
 import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -13,8 +14,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
-app.get('/', (req, res) => {
-    res.send("API Working");
-})
+/**
+ * API ENDPOINTS
+ */
+app.get('/', (req, res) => res.send("API Working"));
+app.use('/api/auth', authRouter)
 
 app.listen(port, ()=> console.log(`server started on PORT: ${port}`));
